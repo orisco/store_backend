37970649628c57d10571699c8e4c0ca743ec0785
Ori Sayag <sayagori@gmail.com>
2021-03-31 16:37:49 +0300

    deployment

--- index.js
+++ index.js
@@ -12,12 +12,25 @@ load()
 app.use(cors())
 app.use(express.json())
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/client'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/client/index.html'));
+});
+
+
 // ROUTES
 app.use('/user', require("./routes/user.routes"))
 app.use('/auth', require("./routes/auth.routes"))
 app.use('/checkout', require("./routes/checkout.routes"))
 app.use('/product', require("./routes/product.routes"))
 app.use('/cart', require("./routes/cart.routes"))
+
+
+
+
 
 app.listen(5000, () => {
   console.log("server is running")
